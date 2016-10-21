3b6b880284ed1bb6e2531789d4d304bf01d4a4d3
Robert Grider <robert.grider@northwestern.edu>
2016-10-21 18:10:44 -0500

    ToolsMenu groups items

--- netlogo-gui/src/main/app/ToolsMenu.scala
+++ netlogo-gui/src/main/app/ToolsMenu.scala
@@ -5,7 +5,7 @@ package org.nlogo.app
 /** note that multiple instances of this class may exist as there are now multiple frames that
  each have their own menu bar and menus ev 8/25/05 */
 
-import javax.swing.{ Action, JMenuItem }
+import javax.swing.{ Action, JMenuItem, JSeparator }
 
 import org.nlogo.core.{ AgentKind, I18N }
 import org.nlogo.swing.UserAction
@@ -18,8 +18,29 @@ class ToolsMenu
 
   setMnemonic('T')
 
-  // this will need to be refined to take groups into account
+  var groups: Map[String, Range] = Map()
+
   def offerAction(action: Action): Unit = {
-    add(new JMenuItem(action), 0)
+    val actionGroup = action.getValue(UserAction.ActionGroupKey) match {
+      case s: String => s
+      case _         => "UndefinedGroup"
+    }
+    if (groups.isEmpty) {
+      add(new JMenuItem(action), 0)
+      groups = groups + (actionGroup -> (0 to 0))
+    } else if (! groups.isDefinedAt(actionGroup)) {
+      addSeparator()
+      val index = getMenuComponentCount
+      add(new JMenuItem(action), index)
+      groups = groups + (actionGroup -> (index to index))
+    } else {
+      val range = groups(actionGroup)
+      add(new JMenuItem(action), range.end)
+      groups = groups.map {
+        case (k, v) if k == actionGroup      => k -> (range.start to range.end + 1)
+        case (k, v) if v.start > range.start => k -> ((v.start + 1) to (v.end + 1))
+        case kv                              => kv
+      }
+    }
   }
 }
